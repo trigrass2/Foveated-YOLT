1c8df52c6b9433c0ca052033d05ccfefe14e9698
Pipa007 <afalmeida.pt@gmail.com>
2016-12-20 22:18:42 +0000

    Compilação dos ficheiros

---
+++ build/CMakeFiles/yolt.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/yolt.dir/src/main.cpp.o"
+  "yolt.pdb"
+  "yolt"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/yolt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
