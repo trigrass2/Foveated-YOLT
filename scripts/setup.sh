dbc05e10b6a3fd1048dd4681ddd32d81d60c86d1
filipa <afalmeida.pt@gmail.com>
2016-12-29 18:12:20 +0000

    add pre-trained models to setup bash

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,9 +4,19 @@
 
 function get_network_weights
 {
-	#download caffenet pre-trained network model
+	#download pre-trained network model
         cd files
+
+	# Caffenet
 	wget http://dl.caffe.berkeleyvision.org/bvlc_reference_caffenet.caffemodel
+	
+	# GoogLeNet
+	wget http://dl.caffe.berkeleyvision.org/bvlc_googlenet.caffemodel
+	
+
+	# VGGNet 16 weight layers
+	wget http://www.robots.ox.ac.uk/~vgg/software/very_deep/caffe/VGG_ILSVRC_16_layers.caffemodel
+
 	cd ..
 }
 
